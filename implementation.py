43555f634d19756d45791ff9b848b3e65e2423d2
Lucy Ruiz <lucyruiz846@gmail.com>
2022-09-07 13:29:48 -0700

    feat: Task 1 completed in implementation file

---
+++ implementation.py
@@ -0,0 +1,34 @@
+#Task 1
+
+def pi_day():
+    months = ('January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December')
+    pi_day = months[2]
+    print(pi_day)
+
+pi_day()
+
+def fruits_veggies():
+    fruits_and_veggies = {'guava', 'pitaya', 'lime', 'kiwi', 'grapes'}
+    fruits_and_veggies_2 = {'pineapple', 'pomegranate', 'sweet potato', 'artichoke'}
+    all_fruits_and_veggies = fruits_and_veggies.union(fruits_and_veggies_2)
+    for items in all_fruits_and_veggies:
+        print(items)
+
+fruits_veggies()
+
+
+def user_profile():
+    user_profile = {
+        'First name': 'Jean',
+        'Last name': 'Sibelius',
+        'Email address':'jean.sib@proton.com',
+        'Phone number': 2067897899
+    }
+
+    print(f'''
+    First name: {user_profile['First name']} 
+    Last name: {user_profile['Last name']}.
+    Email address: {user_profile['Email address']}
+    Phone number: {user_profile['Phone number']}
+    ''')
+user_profile()
